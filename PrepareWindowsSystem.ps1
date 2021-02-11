9545872435b864266f3775960bcefb7648766704
Anthony Magrene <ekfhalomaster@gmail.com>
2021-02-10 21:23:01 -0500

    Create PrepareWindowsSystem.ps1
    -Script to enable WINRM , enable ps-remoting and enable Remote Desktop Protocol.

---
+++ PrepareWindowsSystem.ps1
@@ -0,0 +1,17 @@
+﻿$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    winrm quickconfig
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+    Enable-PSRemoting -Force
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value '*'
+    Set-ItemProperty -path "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name 'fDenyTSConnections' -Value 0
+    Set-Service -Name WinRM -StartupType Automatic
+    Set-Service -Name Winmgmt -StartupType Automatic
+    Start-Service WinRM
+    Start-Service Winmgmt
+
+}
+else{
+    Write-Output "Please Run Powershell As Administrator."
+}
+
